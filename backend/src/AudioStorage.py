a44d7d5f79d2b67a5dc30e7efce6fa013b765381
Ivan Posti <ivmposti@gmail.com>
2019-10-27 13:20:19 +0300

    Add AudioStorage

---
+++ backend/src/AudioStorage.py
@@ -0,0 +1,34 @@
+import os
+import re
+
+
+class AudioStorage:
+    def __init__(self, samples_dir):
+        self.samples = {}
+        for root, _, files in os.walk(samples_dir):
+            for filename in files:
+                if filename.endswith('.mp3'):
+                    basename = os.path.splitext(filename)[0]
+                    target_path = os.path.join(root, filename)
+                    key = self._parse_filename(basename)
+                    self.samples[key] = self._load_file(target_path)
+                else:
+                    print(f'Ignoring non .mp3 file: {filename}')
+
+    @staticmethod
+    def _load_file(filename):
+        with open(filename, mode='rb') as file:
+            return file.read()
+
+    @staticmethod
+    def _parse_filename(filename):
+        matches = re.match(r'([a-zA-Z]+)([0-9]+)', filename)
+        return matches.group(1), matches.group(2)
+
+
+def test_stuff():
+    storage = AudioStorage('../audio/acoustic_grand_piano-mp3')
+    pass
+
+
+# test_stuff()
