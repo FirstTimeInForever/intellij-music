b620da0cdd26e85fe282cdac763a6d995fd3fc71
Artem Bobrov <artembobrov3059@gmail.com>
2019-10-27 14:09:32 +0300

    fix: path fix

--- backend/src/controller/AppController.py
+++ backend/src/controller/AppController.py
@@ -1,18 +1,15 @@
 from src.player.AudioPlayer import AudioPlayer
 from src.player.AudioStorage import AudioStorage
-import sys
 
 
 class AppController:
     def __init__(self):
-        self.audio_storage = AudioStorage('../audio/acoustic_grand_piano-mp3')
+        self.audio_storage = AudioStorage('../../audio/acoustic_grand_piano-mp3')
         self.audio_player = AudioPlayer()
 
     def play_some(self):
-        print(sys.path)
-        print(self.audio_storage.samples.keys())
-        # sound = self.audio_storage.samples[('G', 3)]
-        # self.audio_player.play_sound(sound)
+        sound = self.audio_storage.samples[('G', 3)]
+        self.audio_player.play_sound(sound)
 
 
 def test_stuff():
