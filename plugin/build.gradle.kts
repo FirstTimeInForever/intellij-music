7f7ca04be74be738cfd94a25e4e14018abf8f22f
Dmitry Murzin <diraria@yandex.ru>
2019-10-30 00:34:53 +0300

    Increase heap size

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.intellij.tasks.RunIdeTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -34,4 +35,7 @@ tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml
 }
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+tasks.withType<RunIdeTask> {
+    jvmArgs("-Xmx1536m")
 }
