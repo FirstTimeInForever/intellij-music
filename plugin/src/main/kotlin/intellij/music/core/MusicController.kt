78af85cecf6722c8f3614d2db4c749a508556857
Dmitry Murzin <diraria@yandex.ru>
2019-10-29 19:01:11 +0300

    Download sound font automatically

--- plugin/src/main/kotlin/intellij/music/core/MusicController.kt
+++ plugin/src/main/kotlin/intellij/music/core/MusicController.kt
@@ -3,7 +3,6 @@ package intellij.music.core
 import intellij.music.ui.MusicConfig
 import intellij.music.ui.MusicKeyboardEvent
 import java.io.File
-import java.nio.file.Paths
 
 /*
     Base controller class.
@@ -11,13 +10,27 @@ import java.nio.file.Paths
 class MusicController {
     private var config = MusicConfig.instance
     private val keyboardStorage: KeyboardStorage = KeyboardStorage(10.0)
-    private val userDirectoryPath = Paths.get(System.getProperty("user.home"), "my-midis")
-    private val userFiles = UserDirectoryLoader(userDirectoryPath.toFile())
-    private val midiBackend: MidiBackend = MidiBackend(userFiles.soundFontFile!!)
-    private val midiFileController = MidiFileController(midiBackend, keyboardStorage, userFiles.userFiles)
-    private val randomNotesController = RandomNotesController(midiBackend)
+    private val userFiles = UserDirectoryLoader()
+    private var isSoundFontLoaded: Boolean = false
+
+    private lateinit var midiBackend: MidiBackend
+    private lateinit var midiFileController: MidiFileController
+    private lateinit var randomNotesController: RandomNotesController
+
+    init {
+        userFiles.initSoundFont(::onSoundFontLoaded)
+    }
+
+    private fun onSoundFontLoaded(soundfontFile: File) {
+        midiBackend = MidiBackend(soundfontFile)
+        midiFileController = MidiFileController(midiBackend, keyboardStorage, userFiles.userFiles)
+        randomNotesController = RandomNotesController(midiBackend)
+        isSoundFontLoaded = true
+    }
 
     fun keyboardPressed(event: MusicKeyboardEvent) {
+        if (!isSoundFontLoaded) return
+
         when (config.algorithmType) {
             MusicAlgorithmType.RANDOM -> randomNotesController.keyboardPressed(event)
             MusicAlgorithmType.SEQUENTIAL -> midiFileController.keyboardPressed(event)
--- plugin/src/main/kotlin/intellij/music/core/UserDirectoryLoader.kt
+++ plugin/src/main/kotlin/intellij/music/core/UserDirectoryLoader.kt
@@ -1,34 +1,63 @@
 package intellij.music.core
 
+import com.intellij.openapi.progress.PerformInBackgroundOption
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.progress.Task
 import java.io.File
-import java.lang.RuntimeException
-import java.nio.file.Paths
+import java.io.FileOutputStream
+import java.net.URL
+import java.nio.channels.Channels
 
-class UserDirectoryLoader(private val userDirectory: File) {
+class UserDirectoryLoader {
+    private val userDirectory = File(System.getProperty("user.home"), "my-midis")
+    private val pluginDirectory = File(System.getProperty("user.home"), ".intellij-fancy-music-plugin")
+    private val soundFontFile = File(pluginDirectory, "soundfont.sf2")
     val userFiles = mutableListOf<File>()
-    var soundFontFile: File? = null
 
     init {
-        System.out.println("User directory path: " + userDirectory.toString())
-        if(!userDirectory.exists()) {
-//            System.out.println("User directory does not exists. Trying to create it.")
-//            if(!userDirectory.mkdirs()) {
-//                throw RuntimeException("Could not create user directory")
-//            }
-            throw RuntimeException("User directory does not exists!")
+        println("User directory path: $userDirectory")
+        if (!userDirectory.exists() && !userDirectory.mkdirs()) {
+            throw RuntimeException("Could not create user directory  ($userDirectory)")
         }
-        soundFontFile = Paths.get(userDirectory.toString(), "soundfont.sf2").toFile()
-        if(!soundFontFile!!.exists()) {
-            throw RuntimeException("Could not load soundfont!")
+        if (!pluginDirectory.exists() && !pluginDirectory.mkdirs()) {
+            throw RuntimeException("Could not create plugin directory  ($pluginDirectory)")
         }
+
         reindexFiles()
+    }
+
+    fun initSoundFont(onSoundFontLoaded: (File) -> Unit) {
+        if (soundFontFile.exists()) {
+            onSoundFontLoaded(soundFontFile)
+        } else {
+            loadFontFile(onSoundFontLoaded)
+        }
+    }
+
+    private fun loadFontFile(onSoundFontLoaded: (File) -> Unit) {
+        ProgressManager.getInstance().run(object : Task.Backgroundable(
+            null,
+            "Fancy Music: Downloading sound font...",
+            false,
+            PerformInBackgroundOption.ALWAYS_BACKGROUND
+        ) {
+            override fun run(indicator: ProgressIndicator) {
+                println("Fancy Music: Downloading sound font...")
+                val website = URL("https://raw.githubusercontent.com/urish/cinto/master/media/FluidR3%20GM.sf2")
+                val rbc = Channels.newChannel(website.openStream())
+                val fos = FileOutputStream(soundFontFile)
+                fos.channel.transferFrom(rbc, 0, java.lang.Long.MAX_VALUE)
+                onSoundFontLoaded(soundFontFile)
+            }
+        })
     }
 
     fun reindexFiles() {
         userFiles.clear()
         for(file in userDirectory.listFiles()) {
 //            System.out.println(file.extension)
-            if(!file.isDirectory() && file.extension.equals("mid")) {
+            if(!file.isDirectory && file.extension == "mid") {
                 userFiles.add(file)
             }
         }
