7686c2a1e11bab53cb9ce05ea7100133f0d4d049
Nikolai Sviridov <34921161+NikolaiSviridov@users.noreply.github.com>
2019-10-29 21:07:34 +0300

    Add midi directory textfield (#2)

--- plugin/src/main/kotlin/intellij/music/ui/MusicConfig.kt
+++ plugin/src/main/kotlin/intellij/music/ui/MusicConfig.kt
@@ -6,6 +6,7 @@ import com.intellij.openapi.components.State
 import com.intellij.openapi.components.Storage
 import com.intellij.util.xmlb.XmlSerializerUtil
 import intellij.music.core.MusicAlgorithmType
+import java.io.File
 
 @State(name = "MusicConfig", storages = [(Storage("MusicConfig.xml"))])
 class MusicConfig : PersistentStateComponent<MusicConfig> {
@@ -13,7 +14,8 @@ class MusicConfig : PersistentStateComponent<MusicConfig> {
     var enabled: Boolean = true
     var onlyInEditor: Boolean = false
     var algorithmType: MusicAlgorithmType = MusicAlgorithmType.RANDOM
-    
+    var midiDir: String = File(System.getProperty("user.home"), "my-midis").toString()
+
     override fun getState(): MusicConfig = this
 
     override fun loadState(state: MusicConfig) = XmlSerializerUtil.copyBean(state, this)
--- plugin/src/main/kotlin/intellij/music/ui/MusicConfigurableGUI.kt
+++ plugin/src/main/kotlin/intellij/music/ui/MusicConfigurableGUI.kt
@@ -1,10 +1,14 @@
 package intellij.music.ui
 
+import com.intellij.openapi.fileChooser.FileChooser
+import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory
+import com.intellij.openapi.ui.TextFieldWithBrowseButton
+import com.intellij.openapi.vfs.VirtualFileManager
 import intellij.music.core.MusicAlgorithmType
-import intellij.music.ui.MusicConfig
-import javax.swing.JCheckBox
-import javax.swing.JPanel
-import javax.swing.JRadioButton
+import org.jetbrains.annotations.NonNls
+import java.awt.Component
+import java.io.File
+import javax.swing.*
 
 class MusicConfigurableGUI {
     lateinit var rootPanel: JPanel
@@ -13,14 +17,54 @@ class MusicConfigurableGUI {
 
     private lateinit var musicType1: JRadioButton
     private lateinit var musicType2: JRadioButton
+    private lateinit var midiDirChoose: TextFieldWithBrowseButton
+
+
+    init {
+        midiDirChoose.addActionListener { e ->
+            @NonNls val path = midiDirChoose.getText().trim({ it <= ' ' })
+            selectDirectory(
+                path,
+                {s -> midiDirChoose.setText(s) },
+                rootPanel
+            )
+        }
+    }
+
+
+    fun selectDirectory(
+        path: String,
+        dirConsumer: (String) -> Unit,
+        component: Component?
+    ) {
+        var path = path
+        val descriptor = FileChooserDescriptorFactory.createSingleFolderDescriptor()
+            .withTitle("dialog.title.select.configuration.directory")
+            .withDescription("dialog.description.select.configuration.directory")
+            .withShowFileSystemRoots(true)
+            .withHideIgnored(false)
+            .withShowHiddenFiles(true)
+
+        path = "file://" + path.replace(File.separatorChar, '/')
+        val root = VirtualFileManager.getInstance().findFileByUrl(path)
+
+        val file = FileChooser.chooseFile(descriptor, component, null, root) ?: return
+        val resultPath = file.path.replace('/', File.separatorChar)
+        dirConsumer(resultPath)
+    }
+
 
     fun isModified(config: MusicConfig): Boolean {
         return enabled.isSelected != config.enabled
                 || onlyInEditor.isSelected != config.onlyInEditor
                 || musicType1.isSelected != config.algorithmType.isRandom()
+                || midiDirChoose.text != config.midiDir
     }
 
+
     fun loadFromConfig(config: MusicConfig) {
+        midiDirChoose.setText(config.midiDir)
+
         enabled.isSelected = config.enabled
         onlyInEditor.isSelected = config.onlyInEditor
         onlyInEditor.isEnabled = config.enabled
@@ -33,6 +77,7 @@ class MusicConfigurableGUI {
         }
     }
 
+
     fun saveToConfig(config: MusicConfig) {
         config.enabled = enabled.isSelected
         config.onlyInEditor = onlyInEditor.isSelected
@@ -41,5 +86,7 @@ class MusicConfigurableGUI {
         } else {
             MusicAlgorithmType.SEQUENTIAL
         }
+
+        config.midiDir = midiDirChoose.text
     }
 }
