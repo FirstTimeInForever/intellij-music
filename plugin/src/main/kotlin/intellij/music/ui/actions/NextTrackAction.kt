12666882fccc61c9d480f8801aa89d38b8bd2d38
Dmitry Murzin <diraria@yandex.ru>
2019-10-29 23:56:55 +0300

    Disable NextTrackAction if music type is not sequential

--- plugin/src/main/kotlin/intellij/music/ui/actions/NextTrackAction.kt
+++ plugin/src/main/kotlin/intellij/music/ui/actions/NextTrackAction.kt
@@ -2,9 +2,17 @@ package intellij.music.ui.actions
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import intellij.music.core.MusicAlgorithmType
 import intellij.music.ui.MusicApplicationComponent
+import intellij.music.ui.MusicConfig
 
-class NextTrackAction: AnAction() {
+class NextTrackAction : AnAction() {
+    private val config = MusicConfig.instance
+
+    override fun update(event: AnActionEvent) {
+        event.presentation.isEnabled = config.algorithmType == MusicAlgorithmType.SEQUENTIAL
+    }
+
     override fun actionPerformed(e: AnActionEvent) {
         MusicApplicationComponent.instance.controller.nextTrack()
     }
