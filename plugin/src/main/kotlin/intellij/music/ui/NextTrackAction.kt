748c09d9e58dc3ca31dcb63e5650f0b821e34e48
Ivan Posti <ivmposti@gmail.com>
2019-10-29 19:43:44 +0300

    Rename MusicNextTrackAction

--- plugin/src/main/kotlin/intellij/music/ui/NextTrackAction.kt
+++ plugin/src/main/kotlin/intellij/music/ui/NextTrackAction.kt
@@ -3,7 +3,7 @@ package intellij.music.ui
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 
-class MusicNextTrackAction: AnAction() {
+class NextTrackAction: AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         MusicApplicationComponent.instance.controller.nextTrack()
     }
