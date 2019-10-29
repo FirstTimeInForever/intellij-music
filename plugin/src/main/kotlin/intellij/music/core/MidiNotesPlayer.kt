3c6e39c117543eee45277bf84efe3018a8200998
Ivan Posti <ivmposti@gmail.com>
2019-10-30 00:45:58 +0300

    Fix notes playback

--- plugin/src/main/kotlin/intellij/music/core/MidiNotesPlayer.kt
+++ plugin/src/main/kotlin/intellij/music/core/MidiNotesPlayer.kt
@@ -1,19 +1,28 @@
 package intellij.music.core
 
+import java.util.*
 import javax.sound.midi.MidiChannel
+import kotlin.concurrent.timerTask
 
 class MidiNotesPlayer(backend: MidiBackend) {
     private val channel: MidiChannel = backend.synthesizer.channels[0]
     private var prevNote: Int = 0
+    private val timer = Timer()
 
 //    init {
 //        channel.noteOn(60, 100)
 //    }
 
-    fun playNote(note: Int, velocity: Int) {
+    fun ensureResetNotes() {
         channel.allNotesOff()
+    }
+
+    fun playNote(note: Int, velocity: Int) {
         channel.noteOn(note, velocity)
         prevNote = note
+        timer.schedule(timerTask {
+            channel.noteOff(note)
+        }, velocity.toLong())
     }
 
     fun playChord(notes: List<Int>, velocity: Int) {
