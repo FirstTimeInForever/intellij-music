9e0a6465e6325d762047ea6e33cfff11f36e250e
Dmitry Murzin <diraria@yandex.ru>
2019-11-30 21:20:56 +0300

    Update to 2019.3

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,14 +1,15 @@
+import org.jetbrains.intellij.tasks.PublishTask
 import org.jetbrains.intellij.tasks.RunIdeTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.jetbrains.intellij") version "0.4.10"
+    id("org.jetbrains.intellij") version "0.4.14"
     java
-    kotlin("jvm") version "1.3.50"
+    kotlin("jvm") version "1.3.61"
 }
 
 group = "intellij.music"
-version = "1.0.0"
+version = "1.0.1"
 
 repositories {
     jcenter()
@@ -22,12 +23,14 @@ dependencies {
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-    version = "2019.2.3"
+    version = "2019.3"
 }
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
 }
 tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
+    sinceBuild("182")
+    untilBuild("300")
     changeNotes("""Initial version""")
 }
 tasks.withType<KotlinCompile> {
@@ -35,4 +38,8 @@ tasks.withType<KotlinCompile> {
 }
 tasks.withType<RunIdeTask> {
     jvmArgs("-Xmx1536m")
+}
+
+tasks.withType<PublishTask> {
+    token(System.getenv("ORG_GRADLE_PROJECT_intellijPublishToken"))
 }
